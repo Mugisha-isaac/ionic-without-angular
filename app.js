bf7c3af564e465baf3ee5810037609682a0ca1bd
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-01 10:54:25 +0200

    added the logic part

---
+++ app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const addBtn = document.querySelector('#btn-add');
+
+addBtn.addEventListener('click',()=>{
+    console.log('expense added');
+})
