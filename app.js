4d068632e546820893b5003307ad5d0af1d56470
Mugisha-isaac <mugishaisaac2001example.com>
2021-05-01 11:01:24 +0200

    added some input validations

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const addBtn = document.querySelector('#btn-add');
 
 addBtn.addEventListener('click',()=>{
-    console.log('expense added');
+    const enterdReason = reasonInput.value;
+    const  enterdAmount = amountInput.value;
+
+    if(enterdReason.trim().length<=0 || enterdAmount<=0 || enterdAmount.trim().length<=0){
+        return;
+    }
+    console.log(enterdReason,enterdAmount);
 })
